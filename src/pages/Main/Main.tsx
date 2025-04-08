60c5f531e604ac2c9efa4339ae6aad062b489c46
Anna <blinova.anneta@gmail.com>
2025-04-08 11:04:26 +0200

    test

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,10 +3,9 @@ import s from './Main.module.css'
 import { useGetNewsQuery } from '@/api/newsApi'
 
 export const Main = () => {
-  const {data: news, error, isLoading} = useGetNewsQuery()
+  const {data: news, isLoading} = useGetNewsQuery()
 
   if(isLoading) return <div>Loading...</div>
-  // if(error) return <div>Error: {error.message}</div>
 
   return (
     <main className={s.main}>
